e1ce8cb6c09a97a9a8e322558f77789e6ef5df37
Nicolas Saucedo <nicolas.saucedo@wunderman.com>
2019-11-01 17:58:53 -0300

    comentando todo menos header

--- themes/abogado/index.php
+++ themes/abogado/index.php
@@ -2,7 +2,7 @@
 	<?php get_header(); ?>
   	<!-- Banner Home -->
   	<div class="container my-5 carousel-contenedor">
-		<?php //include('single-slider_for_banner.php') ?>
+		<?php include('single-slider_for_banner.php') ?>
   	</div>
 		<!-- Fin Banner Home -->
   	<!-- Contenido -->
@@ -30,4 +30,4 @@
 			</div>
 		</div>
   	<!-- Fin Contenido -->
-  	<?php //get_footer(); ?>
+  	<?php get_footer(); ?>
